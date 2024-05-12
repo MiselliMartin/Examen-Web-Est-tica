2719d9a745b086a05121ca0ae0c00f98b9578e97
Martín Miselli <martin.miselli@gmail.com>
2024-05-12 12:18:49 -0300

    primer commit funcionalidad principal

--- scripts/dues.js
+++ scripts/dues.js
@@ -3,17 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const dues = document.getElementById("dues");
     const how = document.getElementById("how");
     const trend = document.getElementById("trend");
+    const cash = document.getElementById("cash");
+    const priceInInstalments = document.getElementById("price-in-instalments");
+    const inflation = document.getElementById("inflationRate")
+    const sectionResult = document.getElementById("result")
+    const cashResult = document.getElementById("cashResult")
+    const instalmentsResult = document.getElementById("instalmentsResult")
+    const rigthOption = document.getElementById("rightOption")
 
-    
+    const calcularValorActual = (valorFuturo, tasa, meses) => {
+        return valorFuturo / Math.pow(1 + tasa/100, meses);
+    }
 
-    form.addEventListener("change", () => {
-
-        if (how.value === 'month') { 
-            trend.style.display = "none";
-        }
-
-        let duesValue = dues.value;
-        let howValue = how.value;
+    const inputsMensuales = (howValue, duesValue) => {
         let monthlyInflationInputs = document.getElementById("monthlyInflationInputs");
         monthlyInflationInputs.innerHTML = "";
         
@@ -23,8 +25,54 @@ document.addEventListener("DOMContentLoaded", () => {
                 input.type = "number";
                 input.name = "inflation-for-month-" + i;
                 input.placeholder = "Inflación para la cuota " + i;
+                input.classList.add("inflation-input");
                 monthlyInflationInputs.appendChild(input);
             }
         }
+
+        const inputsInflation = document.querySelectorAll(".inflation-input");
+    }
+
+    form.addEventListener("change", () => {
+
+        if (how.value === 'month') { 
+            trend.style.display = "none";
+        } else {
+            trend.style.display = "block";
+        }
+
+        let duesValue = dues.value;
+        let howValue = how.value;
+        
+
+        inputsMensuales(howValue, duesValue)
+        
     });
+
+    form.addEventListener("submit", () => {
+        let trendValue = trend.value;
+        let cashValue = cash.value;
+        let priceInInstalmentsValue = priceInInstalments.value;
+        let inflationValue = inflation.value;
+        let duesValue = dues.value;
+        let howValue = how.value;
+
+        event.preventDefault();
+        let valorCuota = priceInInstalmentsValue / duesValue
+        let totalValueInstalments = 0;
+
+        if (howValue === "actual") {
+            for (let i = 1; i <= duesValue; i++) {
+                let precioActualCuota = calcularValorActual(valorCuota, inflationValue, i)
+                totalValueInstalments += precioActualCuota
+            }
+
+        }
+
+        sectionResult.style.display = 'block'
+        instalmentsResult.textContent = `Precio en cuotas actualizado: ${totalValueInstalments}`
+
+        form.style.dysplay = 'none'
+        
+    })
 });

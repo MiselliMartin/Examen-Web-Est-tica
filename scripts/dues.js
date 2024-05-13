1b65a5c593e017969288fbfe465fe4c8e2f35b86
Martín Miselli <martin.miselli@gmail.com>
2024-05-13 20:31:44 -0300

    Mostrar porcentaje y cantidad de ahorro o de gasto en exceso

--- scripts/dues.js
+++ scripts/dues.js
@@ -10,6 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const cashResult = document.getElementById("cashResult")
     const instalmentsResult = document.getElementById("instalmentsResult")
     const rigthOption = document.getElementById("rightOption")
+    const explainRigthOption = document.getElementById("explainRigthOption")
+    const expectative = document.getElementById("expectative")
 
     const calcularValorActual = (valorFuturo, tasa, meses) => {
         return valorFuturo / Math.pow(1 + tasa/100, meses);
@@ -30,11 +32,10 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
 
-        const inputsInflation = document.querySelectorAll(".inflation-input");
+        
     }
 
     form.addEventListener("change", () => {
-
         if (how.value === 'month') { 
             trend.style.display = "none";
         } else {
@@ -50,12 +51,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     form.addEventListener("submit", () => {
-        let trendValue = trend.value;
-        let cashValue = cash.value;
-        let priceInInstalmentsValue = priceInInstalments.value;
-        let inflationValue = inflation.value;
-        let duesValue = dues.value;
+        let expectativeValue = expectative.value;
+        let cashValue = parseFloat(cash.value);
+        let priceInInstalmentsValue = parseFloat(priceInInstalments.value);
+        let inflationValue = parseFloat(inflation.value);
+        let duesValue = parseInt(dues.value);
         let howValue = how.value;
+        form.style.display = 'none'
 
         event.preventDefault();
         let valorCuota = priceInInstalmentsValue / duesValue
@@ -63,16 +65,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (howValue === "actual") {
             for (let i = 1; i <= duesValue; i++) {
-                let precioActualCuota = calcularValorActual(valorCuota, inflationValue, i)
-                totalValueInstalments += precioActualCuota
+                let tasaInflacionMensual = inflationValue;
+    
+                if (expectativeValue == "up") {
+                    tasaInflacionMensual += 0.003 * (i - 1); 
+                } else if (expectativeValue == "down") {
+                    tasaInflacionMensual -= 0.003 * (i - 1); 
+                }
+    
+                let precioActualCuota = calcularValorActual(valorCuota, tasaInflacionMensual, i);
+                console.log(precioActualCuota)
+                totalValueInstalments += precioActualCuota;
             }
-
+        } else {
+            const inputsInflation = document.querySelectorAll(".inflation-input");
         }
 
         sectionResult.style.display = 'block'
-        instalmentsResult.textContent = `Precio en cuotas actualizado: ${totalValueInstalments}`
-
-        form.style.dysplay = 'none'
+        cashResult.textContent = "Precio al contado: $" + cashValue.toFixed(2);
+        instalmentsResult.textContent = "Precio en cuotas actualizado: $" + totalValueInstalments.toFixed(2);
+        if (totalValueInstalments < cashValue) {
+            rigthOption.textContent = "Te conviene pagar en cuotas!";
+            explainRigthOption.textContent = `Pagando en cuotas ahorras $${(cashValue - totalValueInstalments).toFixed(2)} en plata de hoy... Osea, el ${(((cashValue / totalValueInstalments)-1)*100).toFixed(2)}%! `;
+        } else {
+            rigthOption.textContent = "Te conviene pagar al contado!";
+            explainRigthOption.textContent = `En cuotas pagarías $${(totalValueInstalments - cashValue).toFixed(2)} de más en plata de hoy... Osea, ${(((totalValueInstalments / cashValue)-1)*100).toFixed(2)}% más!`;
+        }
+        
         
     })
 });
